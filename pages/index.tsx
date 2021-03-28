e6d0388c949cfc3d007b63131d3424df9b04c98c
김범서 <sj441888@naver.com>
2021-03-28 19:51:11 +0900

    fix: husky hook 동작하지 않는 문제 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
+import { css } from '@emotion/react'
 import * as React from 'react'
 
-const MainPage = (): JSX.Element => <div>hello!</div>
+const MainPage = (): JSX.Element => (
+  <div
+    css={css`
+      font-weight: bold;
+      font-size: 2em;
+    `}
+  >
+    hello!
+  </div>
+)
 
 export default MainPage
