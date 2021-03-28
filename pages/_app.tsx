bc46cbe5a93bd1b37c37915b170d1f956a690d97
김범서 <sj441888@naver.com>
2021-03-28 19:22:29 +0900

    chore: nextjs,lint,formatter, commintlint 설정 추가

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const MainPage = (): JSX.Element => <div>hello!</div>
+
+export default MainPage
