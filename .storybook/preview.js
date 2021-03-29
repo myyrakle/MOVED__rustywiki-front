1985f46ab69ee81d550991a681c87326a49aae4f
김범서 <sj441888@naver.com>
2021-03-30 02:15:25 +0900

    chore: react-query, axios, material-icon, 스토리북 webpack config추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,18 @@
+import { withNextRouter } from 'storybook-addon-next-router'
 import { muiTheme } from 'storybook-addon-material-ui'
+import { addDecorator } from '@storybook/react'
 import { theme } from '../shared/CustomTheme'
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
 
 export const decorators = [muiTheme(theme)]
+
+addDecorator(
+  withNextRouter({
+    path: '/', // defaults to `/`
+    asPath: '/', // defaults to `/`
+    query: {}, // defaults to `{}`
+    push() {}, // defaults to using addon actions integration, can override any method in the router
+  })
+)
