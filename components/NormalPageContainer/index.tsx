111d278e946947f1e6f19cbe7360319cf904d5ee
김범서 <sj441888@naver.com>
2021-04-02 00:24:21 +0900

    feat: 기본페이지 컨테이너 추가

---
+++ components/NormalPageContainer/index.tsx
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react'
+import { Paper } from '@material-ui/core'
+import * as React from 'react'
+
+interface INormalPageContainerProps {
+  title: React.ReactNode
+}
+
+const NormalPageContainer: React.FunctionComponent<INormalPageContainerProps> = ({
+  title,
+  children,
+}) => {
+  return (
+    <Paper
+      css={css`
+        padding: 20px;
+        min-height: 100vh;
+      `}
+    >
+      <h1
+        css={css`
+          margin-bottom: 32px;
+        `}
+      >
+        {title}
+      </h1>
+      {children}
+    </Paper>
+  )
+}
+
+export default NormalPageContainer
