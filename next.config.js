5aef95701e93584af6d1da67740b299480cc6c2e
김범서 <sj441888@naver.com>
2021-04-04 01:08:32 +0900

    chore: 개발용 프록시 설정

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://125.133.80.144:11111/:path*', // The :path parameter isn't used here so will be automatically passed in the query
+      },
+    ]
+  },
+}
