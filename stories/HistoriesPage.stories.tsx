eff3f85b9c24f9028c548e8dffba4b7552adca40
김범서 <sj441888@naver.com>
2021-04-28 00:16:11 +0900

    doc: 조회 mocking

--- stories/HistoriesPage.stories.tsx
+++ stories/HistoriesPage.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import HistoriesPage from '../pages/d/histories/[pageName]';
+import { ApiProvider } from '../hooks/useApi';
+import fakerStatic from 'faker';
+import { DocHistoryType } from '../libs/api/DocApi';
 
 export default {
   title: 'Pages/Detail/Histories',
@@ -13,3 +16,30 @@ const Template: Story = () => {
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const ApiMocking = () => (
+  <ApiProvider
+    doc={{
+      async getDocumentHistory() {
+        const histories: DocHistoryType[] = Array(10)
+          .fill('')
+          .map(() => ({
+            char_count: fakerStatic.datatype.number(),
+            content: fakerStatic.lorem.paragraphs(3),
+            id: fakerStatic.random.number(),
+            increase: fakerStatic.datatype.number(),
+            reg_utc: Math.round(
+              fakerStatic.datatype.datetime().getTime() / 1000
+            ),
+            revision_number: fakerStatic.datatype.number(),
+            writer_id: fakerStatic.datatype.number(),
+            writer_name: fakerStatic.name.lastName(),
+          }));
+        return { list: histories, total_count: 20 };
+      },
+    }}
+  >
+    <Template />
+  </ApiProvider>
+);
+ApiMocking.args = {};
