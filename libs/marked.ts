906ae3f4ff0cc79dd8454639a8d7cc1401aaa42e
김범서 <sj441888@naver.com>
2021-05-16 07:16:29 +0900

    feat: 각주추가

--- libs/marked.ts
+++ libs/marked.ts
@@ -2,12 +2,14 @@ import unified from 'unified';
 import markdown from 'remark-parse';
 import html from 'remark-html';
 import * as remarkWikiLink from 'remark-wiki-link';
+import footnotes from 'remark-footnotes';
 const md = unified()
   .use(markdown)
   .use(remarkWikiLink.wikiLinkPlugin, {
     hrefTemplate: (permalink: string) => `/d/wiki/${permalink}`,
     aliasDivider: '|',
   })
+  .use(footnotes, { inlineNotes: true })
   .use(html);
 
 export const customMarked = md;
